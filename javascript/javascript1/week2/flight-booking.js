4e17ae947bc38ea6ef1e94f87b5086ab18920012
Abdullahi <awyussuf116@gmail.com>
2020-05-23 15:25:17 +0200

    Added flight booking file

---
+++ javascript/javascript1/week2/flight-booking.js
@@ -0,0 +1,25 @@
+//Flight booking fullname function
+
+function getFullName(firstname, surname) {
+  return firstname + " " + surname;
+}
+
+const fullname = getFullName("Abdullahi", "Yussef");
+
+console.log(fullname); //returns AbdullahiYussef
+
+const fullname1 = getFullName("Abdullahi", "Yussef");
+const fullname2 = getFullName("Abdullahi", "Yussef");
+
+console.log(fullname1);
+console.log(fullname2);
+
+//formal fullname
+function getFullName(firstname, surname, useFormalName = true) {
+  if (useFormalName == true) {
+    return "Lord" + firstname + surname;
+  } else {
+    return firstname + surname;
+  }
+}
+console.log(getFullName("Abdullahi", "Yussef"));
