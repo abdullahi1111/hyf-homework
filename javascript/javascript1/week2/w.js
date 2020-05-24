60f48a91d7bdb6e658fa29db09ed2350fd01ad8a
Abdullahi <62447803+abdullahi1111@users.noreply.github.com>
2020-05-24 10:21:48 +0200

    Delete w.js

--- javascript/javascript1/week2/w.js
+++
@@ -1,6 +0,0 @@
-function getcolor(name) {
-  const colors = ["blue", "red", "yellow", "pink", "black"];
-  const index = name.length & colors.length;
-  return colors[index];
-}
-console.log(getcolor("johns"));
