f8641350a48b4e31782debefe05b8b3154ca80c3
Abdullahi <62447803+abdullahi1111@users.noreply.github.com>
2020-05-24 10:21:02 +0200

    Delete candy-helper.js

--- javascript/javascript1/week2/candy-helper.js
+++
@@ -1,9 +0,0 @@
-//Candy helper
-const boughtCandyPrices = [];
-const amountToSpend = Math.random() * 100;
-
-function addCandy(candyType, weight) {
-  if (candyType === "sweet") {
-    boughtCnadyPrices.push(0.5 * weight);
-  }
-}
